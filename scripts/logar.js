78fad6c3ec15f62307591b80075df005159fa639
HENRIQUE SILVA STOLLE <henrique_stolle@estudante.sesisenai.org.br>
2024-04-17 12:47:20 -0300

    commit 02: Lóca corrigida e Site funcionando normalmente

--- scripts/logar.js
+++ scripts/logar.js
@@ -1,4 +1,5 @@
 var user1 = ['henrique', '12345678']; // nome e senha válidos  
+
 function logar() {
     // verificação nome
     let usuario = document.getElementById('name').value;
@@ -6,6 +7,7 @@ function logar() {
         document.getElementById('span-usuario').textContent = 'Nome sem dados.';
         document.getElementById('span-usuario').style.display = 'block';
         document.getElementById('span-usuario').style.color = 'red';
+        return; // Impede a continuação do processo se o nome estiver em branco
     } else {
         document.getElementById('span-usuario').textContent = '';
         document.getElementById('span-usuario').style.display = 'none';
@@ -13,15 +15,25 @@ function logar() {
 
     // verificação senha
     let senha = document.getElementById('password').value;
-    if (senha.length <= 8) {
+    if (senha.length < 8) {
         document.getElementById('span-senha').textContent = 'Senha Inválida. Digite ao menos 8 caracteres.';
         document.getElementById('span-senha').style.display = 'block';
         document.getElementById('span-senha').style.color = 'red';
-        console.log(senha.length)
+        return; // Impede a continuação do processo se a senha for muito curta
     } else {
         document.getElementById('span-senha').textContent = '';
         document.getElementById('span-senha').style.display = 'none';
-    } 
-    user1.push([usuario, senha]);
-    window.location.href = '../logado.html';
+    }
+
+    // Verifica se o nome de usuário e a senha correspondem aos valores válidos
+    if (usuario === user1[0] && senha === user1[1]) {
+        // Redireciona para a página logado se os dados estiverem corretos
+        window.location.href = '../logado.html';
+    } else {
+        // Exibe uma mensagem de erro se os dados estiverem incorretos
+        document.getElementById('span-login').textContent = 'Nome de Usuário ou Senha Incorreto.';
+        document.getElementById('span-login').style.display = 'block';
+        document.getElementById('span-login').style.color = 'red';
+        return; // Impede a continuação do processo se a senha for muito curta
+    }
 }
